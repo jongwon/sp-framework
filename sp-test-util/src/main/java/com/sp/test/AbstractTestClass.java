593461115165005f8232530419049c7b74f36a02
jongwon <jongwons.choi@gmail.com>
2017-06-08 11:06:19 +0900

    테스트 클래스 유틸을 추가함.

---
+++ sp-test-util/src/main/java/com/sp/test/AbstractTestClass.java
@@ -0,0 +1,51 @@
+package com.sp.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.*;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import javax.transaction.Transactional;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import static java.lang.String.format;
+
+/**
+ * Created by jongwon on 2017. 6. 8..
+ */
+@Slf4j
+public class AbstractTestClass {
+
+    protected static Validator validator;
+
+    @Rule
+    public TestName testName = new TestName();
+
+    @BeforeClass
+    public static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Before
+    public void before() {
+        log.debug(">>>> {} 시작 ", testName.getMethodName());
+    }
+
+    @After
+    public void after() {
+        log.debug("<<<< {} 끝", testName.getMethodName());
+    }
+
+    protected String randomNumGenerate(int digit){
+        String id = ""+java.util.concurrent.ThreadLocalRandom
+                .current()
+                .nextLong((long)Math.pow(10, digit-1), (long)Math.pow(10, digit));
+        log.debug("{} random id generated", id);
+        return id;
+    }
+
+}
